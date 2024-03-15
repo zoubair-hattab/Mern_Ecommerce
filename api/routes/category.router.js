bb5febe1edc39a061de013719c38927ef218ae4a
zoubair-hattab <elhattabzoubair93@gmail.com>
2024-03-15 15:08:17 +0100

    add routes

--- api/routes/category.router.js
+++ api/routes/category.router.js
@@ -1,7 +1,15 @@
 import express from 'express';
 import { adminToken, useryToken } from '../middlewares/verifyToken.js';
+import {
+  deleteCategory,
+  getCategory,
+  updateCategory,
+} from '../controllers/category.controller.js';
 const router = express.Router();
 
+router.get('/getcategories', getCategory);
 router.post('/create-category', useryToken, adminToken, createCategory);
+router.put('/upadate/:categoryId', useryToken, adminToken, updateCategory);
+router.delete('/delete/:categoryId', useryToken, adminToken, deleteCategory);
 
 export default router;
--- api/server.js
+++ api/server.js
@@ -4,15 +4,24 @@ import cookieParser from 'cookie-parser';
 import dotenv from 'dotenv';
 import connectDB from './db/connectDB.js';
 import ErrorHnadler from './middlewares/error.js';
+import authRouters from './routes/auth.router.js';
+import userRouters from './routes/user.router.js';
+import categoryRouters from './routes/category.router.js';
+
 dotenv.config();
 const app = express();
 app.use(express.json());
 app.use(cors());
 app.use(cookieParser());
+connectDB();
+// routers
+app.use('/api/v2/auth', authRouters);
+app.use('/api/v2/user', userRouters);
+app.use('/api/v2/category', categoryRouters);
 
 // Handle error
 app.use(ErrorHnadler);
-connectDB();
+
 app.listen(3000, () => {
   console.log('Server running on port 3000.');
 });
