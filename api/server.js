83779d98da1c646241e3ea988f5842c9c438538c
zoubair-hattab <elhattabzoubair93@gmail.com>
2024-03-22 00:28:10 +0100

    update

--- api/server.js
+++ api/server.js
@@ -19,7 +19,7 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: ['http://localhost:5173'],
+    origin: ['https://mern-ecommerce-1-thai.onrender.com'],
     credentials: true,
   })
 );
