2076cbfecf17d3b7473c13cc18d76d0f6f087b7f
zoubair-hattab <elhattabzoubair93@gmail.com>
2024-03-22 11:34:38 +0100

    upd

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react-swc';
 export default defineConfig({
   server: {
     proxy: {
-      '/api': {
+      '/api/v2': {
         target: 'http://localhost:3000/api/v2',
         secure: false,
       },
